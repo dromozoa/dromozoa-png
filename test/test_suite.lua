c95dea8f92ff951045ec6b4538e19561439964a0
Tomoyuki Fujimori <moyu@dromozoa.com>
2018-05-10 12:21:35 +0900

    テストを修正。

---
+++ test/test_suite.lua
@@ -0,0 +1,69 @@
+-- Copyright (C) 2018 Tomoyuki Fujimori <moyu@dromozoa.com>
+--
+-- This file is part of dromozoa-png.
+--
+-- dromozoa-png is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- dromozoa-png is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with dromozoa-png.  If not, see <http://www.gnu.org/licenses/>.
+
+local png = require "dromozoa.png"
+
+local verbose = os.getenv "VERBOSE" == "1"
+
+local color_types = {
+  ["0g"] = png.PNG_COLOR_TYPE_GRAY;
+  ["2c"] = png.PNG_COLOR_TYPE_RGB;
+  ["3p"] = png.PNG_COLOR_TYPE_PALETTE;
+  ["4a"] = png.PNG_COLOR_TYPE_GRAY_ALPHA;
+  ["6a"] = png.PNG_COLOR_TYPE_RGB_ALPHA;
+}
+
+local interlace_types = {
+  n = png.PNG_INTERLACE_NONE;
+  i = png.PNG_INTERLACE_ADAM7;
+}
+
+for line in io.lines "docs/PngSuite.txt" do
+  local filename = "docs/" .. line
+  if verbose then
+    io.stderr:write(filename, "\n")
+  end
+
+  local feature, interlace_type, color_type, bit_depth = line:match "/(%w%w%w)([ni])(%d[gcpa])(%d%d)%.png$"
+  if not feature then
+    feature, color_type, bit_depth = assert(line:match "/(exif)(%d[gcpa])(%d%d)%.png$")
+    interlace_type = "n"
+  end
+  bit_depth = tonumber(bit_depth)
+  color_type = color_types[color_type]
+  interlace_type = interlace_types[interlace_type]
+
+  local reader = assert(png.reader())
+
+  local handle = assert(io.open(filename, "rb"))
+  reader:set_read_fn(function (n)
+    return handle:read(n)
+  end)
+
+  if feature:find "^x" then
+    local result, message = reader:read_png()
+    assert(not result)
+    if verbose then
+      io.stderr:write(message)
+    end
+  else
+    assert(reader:read_png())
+    assert(reader:get_bit_depth() == bit_depth)
+    assert(reader:get_color_type() == color_type)
+    assert(reader:get_interlace_type() == interlace_type)
+  end
+end
