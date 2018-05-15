d5c390d9d29f97169feb852b5f2235689b83b025
Tomoyuki Fujimori <moyu@dromozoa.com>
2018-05-15 19:48:19 +0900

    リファクタリング。

--- main.cpp
+++ main.cpp
@@ -15,8 +15,6 @@
 // You should have received a copy of the GNU General Public License
 // along with dromozoa-png.  If not, see <http://www.gnu.org/licenses/>.
 
-#include <stddef.h>
-
 #include "common.hpp"
 
 namespace dromozoa {
