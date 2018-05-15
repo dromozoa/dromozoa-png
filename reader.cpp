c5dc6f019415d63a0f6b68cb4c46c13e92ea64c4
Tomoyuki Fujimori <moyu@dromozoa.com>
2018-05-15 19:38:14 +0900

    set_pixel,get_pixelを廃止。

--- reader.cpp
+++ reader.cpp
@@ -264,69 +264,6 @@ namespace dromozoa {
         png_error(self->png(), "row_pointer not prepared");
       }
     }
-
-    void impl_get_pixel(lua_State* L) {
-      reader_handle* self = check_reader_handle(L, 1);
-      png_uint_32 width = png_get_image_width(self->png(), self->info());
-      png_uint_32 height = png_get_image_height(self->png(), self->info());
-      png_uint_32 x = luaX_check_integer<png_uint_32>(L, 2, 1, width) - 1;
-      png_uint_32 y = luaX_check_integer<png_uint_32>(L, 3, 1, height) - 1;
-
-      png_byte bit_depth = png_get_bit_depth(self->png(), self->info());
-      if (bit_depth != 8 && bit_depth != 16) {
-        png_error(self->png(), "unexpected bit_depth (8 or 16 expected)");
-        return;
-      }
-      if (png_get_color_type(self->png(), self->info()) == PNG_COLOR_TYPE_PALETTE) {
-        png_error(self->png(), "PNG_COLOR_TYPE_PALETTE not supported");
-        return;
-      }
-      png_byte channels = png_get_channels(self->png(), self->info());
-      if (channels < 1 || 4 < channels) {
-        png_error(self->png(), "unexpected channels (1, 2, 3 or 4 expected");
-        return;
-      }
-
-      if (png_bytepp row_pointers = png_get_rows(self->png(), self->info())) {
-        png_bytep p = row_pointers[y] + (bit_depth * channels >> 3) * x;
-        if (bit_depth == 8) {
-          luaX_push(L, p[0]);
-          switch (channels) {
-            case 2:
-              luaX_push(L, p[1]);
-              break;
-            case 3:
-              luaX_push(L, p[1]);
-              luaX_push(L, p[2]);
-              break;
-            case 4:
-              luaX_push(L, p[1]);
-              luaX_push(L, p[2]);
-              luaX_push(L, p[3]);
-              break;
-          }
-        } else {
-          // assume network byte order
-          luaX_push(L, p[0] << 8 | p[1]);
-          switch (channels) {
-            case 2:
-              luaX_push(L, p[2] << 8 | p[3]);
-              break;
-            case 3:
-              luaX_push(L, p[2] << 8 | p[3]);
-              luaX_push(L, p[4] << 8 | p[5]);
-              break;
-            case 4:
-              luaX_push(L, p[2] << 8 | p[3]);
-              luaX_push(L, p[4] << 8 | p[5]);
-              luaX_push(L, p[6] << 8 | p[7]);
-              break;
-          }
-        }
-      } else {
-        png_error(self->png(), "row_pointer not prepared");
-      }
-    }
   }
 
   void initialize_reader(lua_State* L) {
@@ -368,7 +305,6 @@ namespace dromozoa {
       luaX_set_field(L, -1, "get_pixel_aspect_ratio", impl_get_pixel_aspect_ratio);
       luaX_set_field(L, -1, "get_rows", impl_get_rows);
       luaX_set_field(L, -1, "get_row", impl_get_row);
-      luaX_set_field(L, -1, "get_pixel", impl_get_pixel);
     }
     luaX_set_field(L, -2, "reader");
   }
